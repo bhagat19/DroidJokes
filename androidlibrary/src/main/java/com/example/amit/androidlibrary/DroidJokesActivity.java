9f3da6849bf7bc268c810a35ca19384daca9392f
Amit <19.amianand@gmail.com>
2016-07-31 21:48:06 +0530

    added Android lib

--- androidlibrary/src/main/java/com/example/amit/androidlibrary/DroidJokesActivity.java
+++ androidlibrary/src/main/java/com/example/amit/androidlibrary/DroidJokesActivity.java
@@ -1,25 +1,32 @@
-package com.udacity.gradle.builditbigger;
+package com.example.amit.androidlibrary;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
 
-import com.example.JokeTeller;
-import com.example.amit.androidlibrary.DroidJokesActivity;
+/**
+ * Created by amit on 30-07-2016.
+ */
+public class DroidJokesActivity extends AppCompatActivity {
 
-
-public class MainActivity extends ActionBarActivity {
+    public final String LOG_TAG = DroidJokesActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.jokesactivity_main);
+        Intent intent = getIntent();
+        Log.v(LOG_TAG,"joke from java lib via android lib"+intent.getStringExtra("joke"));
+
+        Toast.makeText(this,intent.getStringExtra("joke"),Toast.LENGTH_SHORT).show();
     }
-
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -41,16 +48,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void tellJoke(View view){
-        JokeTeller tellJoke = new JokeTeller();
-        String joke = tellJoke.tellAJoke();
-        Intent intent = new Intent(this, DroidJokesActivity.class);
-        intent.putExtra("joke",joke);
-        startActivity(intent);
-
-   //     Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
-    }
-
-
+    */
 }
---
+++ javalibrary/src/main/java/com/example/JokeTeller.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.logging.Logger;
+
+
+import sun.rmi.runtime.Log;
+
+public class JokeTeller {
+    public final String LOG_TAG = JokeTeller.class.getSimpleName();
+
+    public String tellAJoke(){
+        return "A lame joke from java lib via android lib";
+    }
+
+}
